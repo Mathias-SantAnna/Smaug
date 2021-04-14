29339c587935066b86b439ab2b0af3e86be105c4
Mathias-SantAnna <dark_tricolor@hotmail.com>
2021-04-14 01:29:44 +0000

    shi*t this mtf**

--- run.py
+++ run.py
@@ -19,8 +19,14 @@ def about():
 def contact():
     return render_template("contact.html")
 
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "500")),
+        port=int(os.environ.get("PORT", "5000")),
         debug=True)
